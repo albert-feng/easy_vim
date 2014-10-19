b828bb8edeedee7c071b56dff5d0cb795153c43f
albert-feng <fwg1989@gmail.com>
2014-10-19 09:23:23 +0800

    fix theme problem in install

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ fi
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./.vimrc ~
+mkdir -p ~/.vim/colors
+touch ~/.vim/colors/monokai.vim
 vim +PluginInstall +qa
 
 cp -r ~/.vim/bundle/vim-monokai/colors/ ~/.vim/
